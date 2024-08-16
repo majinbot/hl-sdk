f181d592e4572da7338ccece7b7e4f2eab60aa2b
(｡◝‿◜｡) <176598310+majinbot@users.noreply.github.com>
2024-08-16 16:40:57 +0300

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,25 @@
-// Export constants
-export * from './constants';
-
 // Export types
-export type {
-    Tif,
-    Tpsl,
-    LimitOrderType,
-    TriggerOrderType,
-    Grouping,
-    OrderType,
-    Cloid,
-    OidOrCloid,
-    AllMids,
-    Meta,
-    ClearinghouseState,
-    UserFills,
-    OrderResponse,
-    WsTrade,
-    WsBook,
-    WsLevel,
-    WsOrder,
-    WsUserEvent,
-    WsFill,
-    WsUserFunding,
-    WsLiquidation,
-    WsNonUserCancel,
-    SpotClearinghouseState,
-    FrontendOpenOrders,
-    UserRateLimit,
-    OrderStatus,
-    L2Book,
-    CandleSnapshot,
-    AssetCtx,
-    MetaAndAssetCtxs,
-    UserFundingDelta,
-    UserFundingEntry,
-    UserFunding,
-    UserNonFundingLedgerDelta,
-    UserNonFundingLedgerEntry,
-    UserNonFundingLedgerUpdates,
-    FundingHistoryEntry,
-    FundingHistory,
-    SpotToken,
-    SpotMarket,
-    SpotMeta,
-    SpotAssetCtx,
-    SpotMetaAndAssetCtxs,
-    UserOpenOrder,
-    UserOpenOrders,
-    OrderRequest,
-    OrderWire,
-    CancelOrderRequest,
-    CancelOrderRequests,
-    CancelByCloidRequest,
-    ModifyRequest,
-    ModifyWire,
-    ScheduleCancelAction,
-    Signature,
-    Notification,
-    WebData2,
-    Candle,
-    WsUserFill,
-    WsUserFills,
-    WsUserFundings,
-    WsUserNonFundingLedgerUpdate,
-    WsUserNonFundingLedgerUpdates,
-} from './types';
+export * from './types';
+
+// Export REST API components
+export * from './rest/exchange';
+export * from './rest/info';
+export * from './rest/custom';
+export * from './rest/info/general';
+export * from './rest/info/perps';
+export * from './rest/info/spot';
+
+// Export WebSocket components
+export * from './websocket/connection';
+export * from './websocket/subscriptions';
+
+// Export utility functions
+export * from './utils/helpers';
+export * from './utils/rateLimiter';
+export * from './utils/signing';
+export * from './utils/errors';
+
+// Export the main classes
+export * from './api';
+export * from './config';
+export * from './constants';
